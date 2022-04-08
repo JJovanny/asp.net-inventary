1d9c3e32f99e9860b169b0de942fcc0c9368a6fb
Jovanny <palenciagj@gmail.com>
2022-04-08 15:03:55 -0400

    consumiendo api desde front

---
+++ practica1a/Controllers/PruebaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practica1a.Controllers
+{
+
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+
+
+        [HttpGet]
+
+        public IActionResult GetPrueba()
+        {
+
+            return Content("success baby");
+        }
+
+
+    }
+}
