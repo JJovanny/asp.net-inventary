6c04f8f1ad84e0c867de1e9d10b43f157cfaa75c
Jovanny <palenciagj@gmail.com>
2022-04-08 14:08:18 -0400

    prueba

---
+++ practica1a/Controllers/PruebaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practica1a.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult Getdemo()
+        {
+
+            return Content("prueba");
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
